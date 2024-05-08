ed7c42405ea1a639efb744f14eef916104813f72
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-08 22:05:04 +0200

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -29,6 +29,9 @@ elif [[ "$(lsb_release -cs)" == "noble" ]]; then
     echo "building for ubuntu noble minimal"
     sudo apt update 
     sudo apt install -y build-essential flex bc bison dkms
+    sudo apt install -y python3-pip
+    sudo pip install cloudsmith-api --break-system-packages
+    sudo pip install cloudsmith-cli --break-system-packages
     make KSRC=/usr/src/linux-headers-6.8.0-31-generic O="" modules
     mkdir -p package/lib/modules/6.8.0-31-generic/kernel/drivers/net/wireless/
     cp *.ko package/lib/modules/6.8.0-31-generic/kernel/drivers/net/wireless/
